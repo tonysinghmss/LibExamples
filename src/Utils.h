4aa20a9d6396c1211c4fbe944f93a2f8e7a7bc55
Manishankar Singh <manishankarsngh007@gmail.com>
2017-03-14 00:55:55 +0530

    Update Utils.h

--- src/Utils.h
+++ src/Utils.h
@@ -3,7 +3,7 @@
 #include <string>
 #include <vector>
 bool not_url_char(char);
-std::string::const_iterator url_end(std::string::const_iterator, std::const_iterator);
-std::string::const_iterator url_beg(std::string::const_iterator, std::const_iterator);
-vector<string> find_urls(const std::string&);
+std::string::const_iterator url_end(std::string::const_iterator, std::string::const_iterator);
+std::string::const_iterator url_beg(std::string::const_iterator, std::string::const_iterator);
+std::vector<std::string> find_urls(const std::string&);
 #endif

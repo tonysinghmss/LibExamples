7e37cd60d895ba7214b06def0f4803794cfbe2a7
Manishankar Singh <manishankarsngh007@gmail.com>
2017-03-15 18:16:12 +0530

    Update Utils.h
    split method added.

--- src/Utils.h
+++ src/Utils.h
@@ -6,4 +6,5 @@ bool not_url_char(char);
 std::string::const_iterator url_end(std::string::const_iterator, std::string::const_iterator);
 std::string::const_iterator url_beg(std::string::const_iterator, std::string::const_iterator);
 std::vector<std::string> find_urls(const std::string&);
+std::vector<std::string> split(const std::string&)
 #endif

08aeeba0fc484fd925fa74ed28c0794b0ec24157
Manishankar Singh <manishankarsngh007@gmail.com>
2017-03-15 18:15:23 +0530

    Update Utils.cpp
    Add a split method

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -59,3 +59,29 @@ std::string::const_iterator url_end(std::string::const_iterator b, std::string::
     }
     return ret;
   }
+
+ std::vector<std::string> split(const std::string& s){
+    std::vector<std:string> ret;
+    typedef std::string::size_type string_size;
+    string_size i = 0;
+    
+    while(i != s.size()){
+        // ignore leading blanks
+        // invariant: characters in range [original i, current i) are all spaces
+        while(i != s.size() && isspace(s[i]))
+            ++i;
+        
+        // find end of next word
+        string_size j = i;
+        // invariant: none of  the characters in range [original j, current j) is a space
+        while(j != s.size() && !isspace(s[j]))
+            ++j;
+        // if we found some nonwhitespace characters
+        if( i != j){
+            // copy from s starting  at i and taking j - i chars
+            ret.push_back(s.substr(i,j-i));
+            i = j;
+        }
+    }
+    return ret;
+}
